ded19f0959c373485e4106944937b321863381a5
johnson.zhong <514961926@qq.com>
2022-12-19 19:38:03 +0800

    fix: 测试输入时播放动画卡顿的问题

--- ios/Classes/GrowingTextView.swift
+++ ios/Classes/GrowingTextView.swift
@@ -96,35 +96,35 @@ open class GrowingTextView: UITextView {
     override open func layoutSubviews() {
         super.layoutSubviews()
         
-        if text == oldText && bounds.size == oldSize { return }
-        oldText = text
-        oldSize = bounds.size
-        
-        var height = contentSize.height
-        
-        // Constrain minimum height
-        height = minHeight > 0 ? max(height, minHeight) : height
-        
-        // Constrain maximum height
-        height = maxHeight > 0 ? min(height, maxHeight) : height
-        
-        // Add height constraint if it is not found
-        if (heightConstraint == nil) {
-            heightConstraint = NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: height)
-//            addConstraint(heightConstraint!)
-        }
-        
-        // Update height constraint if needed
-        if height != heightConstraint!.constant {
-            shouldScrollAfterHeightChanged = true
-            heightConstraint!.constant = height
-            if let delegate = delegate as? GrowingTextViewDelegate {
-                delegate.textViewDidChangeHeight?(self, height: height)
-            }
-        } else if shouldScrollAfterHeightChanged {
-            shouldScrollAfterHeightChanged = false
-//            scrollToCorrectPosition()
-        }
+//        if text == oldText && bounds.size == oldSize { return }
+//        oldText = text
+//        oldSize = bounds.size
+//
+//        var height = contentSize.height
+//
+//        // Constrain minimum height
+//        height = minHeight > 0 ? max(height, minHeight) : height
+//
+//        // Constrain maximum height
+//        height = maxHeight > 0 ? min(height, maxHeight) : height
+//
+//        // Add height constraint if it is not found
+//        if (heightConstraint == nil) {
+//            heightConstraint = NSLayoutConstraint(item: self, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1.0, constant: height)
+////            addConstraint(heightConstraint!)
+//        }
+//
+//        // Update height constraint if needed
+//        if height != heightConstraint!.constant {
+//            shouldScrollAfterHeightChanged = true
+//            heightConstraint!.constant = height
+//            if let delegate = delegate as? GrowingTextViewDelegate {
+//                delegate.textViewDidChangeHeight?(self, height: height)
+//            }
+//        } else if shouldScrollAfterHeightChanged {
+//            shouldScrollAfterHeightChanged = false
+////            scrollToCorrectPosition()
+//        }
     }
     
     private func scrollToCorrectPosition() {
@@ -139,31 +139,31 @@ open class GrowingTextView: UITextView {
     override open func draw(_ rect: CGRect) {
         super.draw(rect)
         
-        if text.isEmpty {
-            let xValue = textContainerInset.left + textContainer.lineFragmentPadding
-            let yValue = textContainerInset.top
-            let width = rect.size.width - xValue - textContainerInset.right
-            let height = rect.size.height - yValue - textContainerInset.bottom
-            let placeholderRect = CGRect(x: xValue, y: yValue, width: width, height: height)
-            
-            if let attributedPlaceholder = attributedPlaceholder {
-                // Prefer to use attributedPlaceholder
-                attributedPlaceholder.draw(in: placeholderRect)
-            } else if let placeholder = placeholder {
-                // Otherwise user placeholder and inherit `text` attributes
-                let paragraphStyle = NSMutableParagraphStyle()
-                paragraphStyle.alignment = textAlignment
-                var attributes: [NSAttributedString.Key: Any] = [
-                    .foregroundColor: placeholderColor,
-                    .paragraphStyle: paragraphStyle
-                ]
-                if let font = font {
-                    attributes[.font] = font
-                }
-                
-                placeholder.draw(in: placeholderRect, withAttributes: attributes)
-            }
-        }
+//        if text.isEmpty {
+//            let xValue = textContainerInset.left + textContainer.lineFragmentPadding
+//            let yValue = textContainerInset.top
+//            let width = rect.size.width - xValue - textContainerInset.right
+//            let height = rect.size.height - yValue - textContainerInset.bottom
+//            let placeholderRect = CGRect(x: xValue, y: yValue, width: width, height: height)
+//
+//            if let attributedPlaceholder = attributedPlaceholder {
+//                // Prefer to use attributedPlaceholder
+//                attributedPlaceholder.draw(in: placeholderRect)
+//            } else if let placeholder = placeholder {
+//                // Otherwise user placeholder and inherit `text` attributes
+//                let paragraphStyle = NSMutableParagraphStyle()
+//                paragraphStyle.alignment = textAlignment
+//                var attributes: [NSAttributedString.Key: Any] = [
+//                    .foregroundColor: placeholderColor,
+//                    .paragraphStyle: paragraphStyle
+//                ]
+//                if let font = font {
+//                    attributes[.font] = font
+//                }
+//
+//                placeholder.draw(in: placeholderRect, withAttributes: attributes)
+//            }
+//        }
     }
     
     // Trim white space and new line characters when end editing.
